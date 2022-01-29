9f74ad410ac6b2215728ff0d888a91bf6a30a840
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-01-30 00:51:28 +0400

    /

--- Warehouse/base/admin.py
+++ Warehouse/base/admin.py
@@ -1,6 +1,4 @@
-from csv import list_dialects
 from django.contrib import admin
-
 from .models import *
 
 
--- Warehouse/base/views.py
+++ Warehouse/base/views.py
@@ -1,3 +1,13 @@
+from urllib import request
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+
+def home(request):
+    stocks=Stocks.objects.all()
+    # stocks=Stocks.objects.get(P_name='SSD')
+    
+    
+    return render(request,'home.html',{'stocks':stocks})
