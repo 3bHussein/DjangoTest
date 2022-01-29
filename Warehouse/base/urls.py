f5d0a9a92620bd0c0dd1998b24c6fa065ee9ff8a
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-01-30 01:34:03 +0400

    /

--- Warehouse/base/urls.py
+++ Warehouse/base/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     
     path('',views.home),
+    path('stock/<str:pk>',views.stockD,name='stock')
     
 ]
--- Warehouse/base/views.py
+++ Warehouse/base/views.py
@@ -1,6 +1,7 @@
 from urllib import request
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -8,6 +9,20 @@ from .models import *
 def home(request):
     stocks=Stocks.objects.all()
     # stocks=Stocks.objects.get(P_name='SSD')
-    
-    
     return render(request,'home.html',{'stocks':stocks})
+
+
+def stockD(request,pk):
+    try:
+        stock=Stocks.objects.get(P_id=pk)
+    except:
+        return HttpResponse(f'<h1 style="text-align:center">Stock in not availabl</h1>')
+
+        
+   
+   
+    return render(request,'stockd.html',{'stock':stock}) 
+ 
+        
+        
+        
