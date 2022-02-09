8ad7b50412ae1ed0079cfaab84084e7d3836d7c6
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-02-09 15:16:38 +0400

    Update test.py

--- Crash_Course_Python/test.py
+++ Crash_Course_Python/test.py
@@ -1,12 +1,25 @@
-
-
-
-
-
 list1=[1,2,3]
 Tubule={
     'a':'ahmed'
 }
 
 
-print(Tubule['a'])
+# print(Tubule['a'])
+def lucky_number(name):
+    number=len(name)*9
+    print('your lucky number length is'+str(number))
+    
+
+
+
+lucky_number('key')
+lucky_number('cameron')
+number=10
+if number > 11: 
+  print(0)
+elif number != 10:
+  print(1)
+elif number >= 20 or number < 12:
+  print(2)
+else:
+  print(3)
